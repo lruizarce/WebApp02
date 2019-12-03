8422818d1f435c1d6ec636ba5b8465643f1b103a
Luis <lruizarc@msudenver.edu>
2019-12-03 12:03:18 -0700

    Copied Assignment 1 into 2's repo

---
+++ app/models/car.rb
@@ -0,0 +1,6 @@
+class Car < ApplicationRecord
+  belongs_to :make
+  has_many :car_part
+  has_many :parts, through: :car_part
+
+end
---
+++ app/models/make.rb
@@ -0,0 +1,4 @@
+class Make < ApplicationRecord
+  has_many :cars
+
+end
